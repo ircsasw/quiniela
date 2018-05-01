1ae84eadbe37364bff3b4c88a05c332cb99219bc
4ndr3y <4ndr3yp00t@protonmail.com>
2018-05-01 18:12:08 -0500

    He migrado la inserción de imágenes para las banderas en la BDD.

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -12,6 +12,7 @@ use frontend\models\PasswordResetRequestForm;
 use frontend\models\ResetPasswordForm;
 use frontend\models\SignupForm;
 use frontend\models\ContactForm;
+use frontend\models\Teams;
 
 /**
  * Site controller
@@ -72,7 +73,8 @@ class SiteController extends Controller
      */
     public function actionIndex()
     {
-        return $this->render('index');
+        $team = Teams::findOne(22);
+        return $this->render('index', ['team' => $team]);
     }
 
     /**
