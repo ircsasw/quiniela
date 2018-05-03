7c764e111978be09f90c38b4071263ebe72852d5
4ndr3y <4ndr3yp00t@protonmail.com>
2018-05-03 11:44:49 -0500

    He creado role dentro de users listo para un migrate

--- console/migrations/sql/m180503_000941.sql
+++ console/migrations/sql/m180503_000941.sql
@@ -1,5 +1,3 @@
-ALTER TABLE `user` ADD COLUMN `role` INT(6) NULL DEFAULT '10' AFTER `updated_at`;
-
 ALTER TABLE `user`
 	CHANGE COLUMN `role` `role` INT(11) NULL DEFAULT '10' AFTER `status`;
 SELECT `DEFAULT_COLLATION_NAME` FROM `information_schema`.`SCHEMATA` WHERE `SCHEMA_NAME`='quiniela';
