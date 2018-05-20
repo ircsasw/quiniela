b744c2921816c012e0438ec443a25de187163043
Arturo R <ircsasw@gmail.com>
2018-05-20 08:47:15 -0500

    el usuario NO llena su propia quiniela !!

--- frontend/views/soccer-bet/view.php
+++ frontend/views/soccer-bet/view.php
@@ -41,22 +41,9 @@ $this->params['breadcrumbs'][] = $this->title;
         'pjax' => true,
         'columns' => [
             ['class' => 'yii\grid\SerialColumn'],
-            'soccer_bet_id',
             'match_id',
-            [
-                'class' => 'kartik\grid\EditableColumn',
-                'attribute' => 'score_a',
-                'value' => function($model){
-                    return $model->score_a;
-                }
-            ],
-            [
-                'class' => 'kartik\grid\EditableColumn',
-                'attribute' => 'score_b',
-                'value' => function($model){
-                    return $model->score_b;
-                }
-            ],
+            'score_a',
+            'score_b',
             'points',
         ],
     ]); ?>
