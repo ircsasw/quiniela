18827ae039e89b04870b80658d6958928d87727a
AndreyPootMay <AndreyPootMay@github.com>
2018-06-11 18:39:59 -0500

    Los apostadores no podrán borrar su quiniela 24 horas antes del mundial

--- frontend/views/soccer-bet/mybets.php
+++ frontend/views/soccer-bet/mybets.php
@@ -35,8 +35,27 @@ $this->params['breadcrumbs'][] = $this->title;
             [
                 'class' => 'yii\grid\ActionColumn',
                 'template' => '{view} {delete}',
+                'contentOptions' => ['class' => 'action-buttons'],
+                'buttons' => [
+                        'delete' => function ($url, $model) {
+                            $actual_date = strtotime(date('d-m-Y H:i:00', time()));
+                            $start_date = strtotime('13-06-2018 10:00:00');
+                            // $start_date = $actual_date; testing de fecha actual
+                            // Si la fecha actual es menor a un día antes del mundial se puede seguir borrando nuestra quiniela, una vez 24 hrs antes del mundial esta opción queda anulada para que la apuesta prosiga todo el transcurso.
+                            return ($actual_date < $start_date) ? Html::a('<i class="glyphicon glyphicon-remove"></i>',
+                                $url, [
+                                'title' => Yii::t('app', 'Eliminar'),
+                                'class' => 'red',
+                                'data' => [
+                                    'confirm' => Yii::t('app', 'Seguro que quiere eliminar este registro?'),
+                                    'method' => 'post',
+                                ],
+                            ]) : "";
+                        }
+                    ]
+                ],
             ],
-        ],
-    ]); ?>
+        ]); 
+    ?>
 </div>
 
